0e28e7a69d8aaed1940b873bb4f053c9dbf523c9
Angad Tendulkar <angad.tendulkar@gmail.com>
2024-04-28 17:59:49 -0400

    nuke like 50% of the js

--- config.js
+++ config.js
@@ -1,36 +1,9 @@
-import GLib from 'gi://GLib';
+await Utils.execAsync([
+	'bun', 'build', `${App.configDir}/src/main.ts`,
+	'--outfile', '/tmp/ags/main.js',
+	'--external', 'resource://*',
+	'--external', 'gi://*',
+	'--external', 'file://*',
+]); 
 
-const main = '/tmp/ags/main.js';
-const entry = `${App.configDir}/src/main.ts`;
-const bundler = GLib.getenv('AGS_BUNDLER') || 'bun';
-
-try {
-	switch (bundler) {
-		case 'bun': await Utils.execAsync([
-			'bun', 'build', entry,
-			'--outfile', main,
-			'--external', 'resource://*',
-			'--external', 'gi://*',
-			'--external', 'file://*',
-		]); break;
-
-		case 'esbuild': await Utils.execAsync([
-			'esbuild', '--bundle', entry,
-			'--format=esm',
-			`--outfile=${main}`,
-			'--external:resource://*',
-			'--external:gi://*',
-			'--external:file://*',
-		]); break;
-
-		default:
-			throw `"${bundler}" is not a valid bundler`;
-	}
-
-	await import(`file://${main}`);
-} catch (error) {
-	console.error(error);
-	App.quit();
-}
-
-export { };
+await import('file:///tmp/ags/main.js');
