9d530c2b17c48f1466de759f38fbc281a71895a1
Angad Tendulkar <angad.tendulkar@gmail.com>
2024-04-30 09:00:46 -0400

    move css out of subdir * move styles/scss into styles/ * move compiled into tmp dir

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import Bar from '@widgets/bar';
 
-Utils.exec(`sass ${App.configDir}/styles/scss/index.scss ${App.configDir}/styles/.index.css`);
-
+Utils.exec(`sass ${App.configDir}/styles/index.scss /tmp/ags/index.css`);
 App.resetCss();
-App.applyCss(`${App.configDir}/styles/.index.css`);
+App.applyCss('/tmp/ags/index.css');
 
 App.config({
 	windows: [Bar(0)],
